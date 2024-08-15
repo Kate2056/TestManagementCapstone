13775647224dfeb157932b80ec73c67078b27a3b
kbri135 <kbri135@wgu.edu>
2024-08-15 15:09:19 -0500

    Added menus for Admin home page and test case details, ability to save and create test cases

--- app/src/main/java/com/softwareengineering/testmanagementcapstone/UI/AdminHomePage.java
+++ app/src/main/java/com/softwareengineering/testmanagementcapstone/UI/AdminHomePage.java
@@ -2,6 +2,9 @@ package com.softwareengineering.testmanagementcapstone.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.softwareengineering.testmanagementcapstone.MainActivity;
 import com.softwareengineering.testmanagementcapstone.R;
 import com.softwareengineering.testmanagementcapstone.database.Repository;
@@ -36,9 +40,21 @@ public class AdminHomePage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        adminID = getIntent().getIntExtra("admin", -1);
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminHomePage.this, TestCaseDetails.class);
+                intent.putExtra("admin", adminID);
+                startActivity(intent);
+            }
+        });
+
         repository = new Repository(getApplication());
 
-        adminID = getIntent().getIntExtra("admin", -1);
+
         adminLoggedIn = findViewById(R.id.adminLoggedIn);
         for(Admin i : repository.getmAllAdmins()){
             if(i.getUserID() == adminID){
@@ -57,4 +73,33 @@ public class AdminHomePage extends AppCompatActivity {
         recyclerView.setAdapter(testCaseAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         testCaseAdapter.setTestCases(allTests, adminID);
-}}
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        List<TestCase> allTests = repository.getmAllTestCases();
+        final TestCaseAdapter testCaseAdapter = new TestCaseAdapter(this);
+        recyclerView.setAdapter(testCaseAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        testCaseAdapter.setTestCases(allTests, adminID);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.logout){
+            Intent intent = new Intent(AdminHomePage.this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return true;
+    }
+}
--- app/src/main/java/com/softwareengineering/testmanagementcapstone/UI/TestCaseDetails.java
+++ app/src/main/java/com/softwareengineering/testmanagementcapstone/UI/TestCaseDetails.java
@@ -2,7 +2,11 @@ package com.softwareengineering.testmanagementcapstone.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,24 +18,55 @@ import com.softwareengineering.testmanagementcapstone.MainActivity;
 import com.softwareengineering.testmanagementcapstone.R;
 import com.softwareengineering.testmanagementcapstone.database.Repository;
 import com.softwareengineering.testmanagementcapstone.entities.Admin;
+import com.softwareengineering.testmanagementcapstone.entities.TestCase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class TestCaseDetails extends AppCompatActivity {
     TextView adminLoggedIn;
     int adminID;
     Repository repository;
     Admin currentAdmin;
+    String name;
+    String steps;
+    String expected;
+    String created;
+    String updated;
+    int testID;
+    EditText editName;
+    EditText editSteps;
+    EditText editExpected;
+    Date currentDate = new Date();
+    String dateFormat = "MM/dd/yy HH:mm:ss";
+    SimpleDateFormat df = new SimpleDateFormat(dateFormat, Locale.US);
+    String currentDateFormatted = df.format(currentDate);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.test_case_details);
+        setContentView(R.layout.activity_test_case_details);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
         repository = new Repository(getApplication());
+        editName = findViewById(R.id.name);
+        editSteps = findViewById(R.id.steps);
+        editExpected = findViewById(R.id.expected);
+        testID = getIntent().getIntExtra("id", -1);
+        name = getIntent().getStringExtra("name");
+        steps = getIntent().getStringExtra("steps");
+        expected = getIntent().getStringExtra("expected");
+        created = getIntent().getStringExtra("created");
+        updated = getIntent().getStringExtra("updated");
+        editName.setText(name);
+        editSteps.setText(steps);
+        editExpected.setText(expected);
+
 
         adminID = getIntent().getIntExtra("admin", -1);
         adminLoggedIn = findViewById(R.id.adminLoggedIn);
@@ -46,5 +81,56 @@ public class TestCaseDetails extends AppCompatActivity {
             Intent intent = new Intent(TestCaseDetails.this, MainActivity.class);
             startActivity(intent);
         }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_test_case_details, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            this.finish();
+            return true;
+        }
+
+        if(item.getItemId() == R.id.logout){
+            Intent intent = new Intent(TestCaseDetails.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        if(item.getItemId() == R.id.saveTest){
+            TestCase testCase;
+
+            if(testID == -1){
+                if(repository.getmAllTestCases().isEmpty()){
+                    testID = 1;
+                }
+                else{
+                    testID = repository.getmAllTestCases().get(repository.getmAllTestCases().size() - 1).getTestID() + 1;
+                }
+
+                testCase = new TestCase(testID, editName.getText().toString(), editSteps.getText().toString(), editExpected.getText().toString(), created, currentDateFormatted);
+                if(testCase.getName().isEmpty() || testCase.getSteps().isEmpty() || testCase.getExpectedResult().isEmpty()){
+                    Toast.makeText(TestCaseDetails.this, "Please fill out all fields.", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                repository.insert(testCase);
+                Toast.makeText(TestCaseDetails.this, "Test Case Saved!", Toast.LENGTH_LONG).show();
+                this.finish();
+            }
+            else{
+                testCase = new TestCase(testID, editName.getText().toString(), editSteps.getText().toString(), editExpected.getText().toString(), created, currentDateFormatted);
+                if(testCase.getName().isEmpty() || testCase.getSteps().isEmpty() || testCase.getExpectedResult().isEmpty()){
+                    Toast.makeText(TestCaseDetails.this, "Please fill out all fields.", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                repository.update(testCase);
+                Toast.makeText(TestCaseDetails.this, "Test Case Saved!", Toast.LENGTH_LONG).show();
+                this.finish();
+            }
+        }
+
+        return true;
     }
 }
