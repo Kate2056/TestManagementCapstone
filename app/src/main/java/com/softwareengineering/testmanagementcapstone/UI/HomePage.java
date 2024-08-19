e41f45de13f0285801e0f903f18bd64c343993d1
kbri135 <kbri135@wgu.edu>
2024-08-18 19:31:48 -0500

    Added search functionality to home page

--- app/src/main/java/com/softwareengineering/testmanagementcapstone/UI/HomePage.java
+++ app/src/main/java/com/softwareengineering/testmanagementcapstone/UI/HomePage.java
@@ -4,6 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -21,14 +27,22 @@ import com.softwareengineering.testmanagementcapstone.entities.TestCase;
 import com.softwareengineering.testmanagementcapstone.entities.TestResult;
 import com.softwareengineering.testmanagementcapstone.entities.User;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     TextView userLoggedIn;
     int userID;
     Repository repository;
     User currentUser;
-
+    Spinner spinner;
+    EditText editCriteria;
+    String criteria;
+    String searchBy = "all";
+    String[] spinnerItems = new String[]{"Search All", "Name", "Steps", "Expected", "Created", "Updated"};
+    ImageButton search;
+    List<TestCase> tests;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +53,25 @@ public class HomePage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        editCriteria = findViewById(R.id.criteria);
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(HomePage.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, spinnerItems);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+        search = findViewById(R.id.searchButton);
         repository = new Repository(getApplication());
+        tests = repository.getmAllTestCases();
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                criteria = editCriteria.getText().toString();
+                tests = searchTests(criteria, searchBy);
+                onResume();
+            }
+        });
+
+
         userID = getIntent().getIntExtra("user", -1);
         userLoggedIn = findViewById(R.id.userLoggedIn);
 
@@ -62,12 +94,40 @@ public class HomePage extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         testCaseAdapter.setTestCases(allTests, userID);
     }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id){
+        switch(position){
+            case 0:
+                searchBy = "all";
+                break;
+            case 1:
+                searchBy = "name";
+                break;
+            case 2:
+                searchBy = "steps";
+                break;
+            case 3:
+                searchBy = "expected";
+                break;
+            case 4:
+                searchBy = "create";
+                break;
+            case 5:
+                searchBy = "updated";
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+            searchBy = "all";
+    }
 
     @Override
     protected void onResume(){
         super.onResume();
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        List<TestCase> allTests = repository.getmAllTestCases();
+        List<TestCase> allTests = tests;
         final TestHomeAdapter testHomeAdapter = new TestHomeAdapter(this);
         recyclerView.setAdapter(testHomeAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -97,5 +157,55 @@ public class HomePage extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    public List<TestCase> searchTests(String criteria, String selection){
+        List<TestCase> allTests = repository.getmAllTestCases();
+        List<TestCase> toReturn = new ArrayList<>();
+        criteria = criteria.toLowerCase();
+
+        if (Objects.equals(selection, "all")) {
+            for(TestCase t : allTests){
+                if(t.getName().toLowerCase().contains(criteria) || t.getSteps().toLowerCase().contains(criteria) || t.getExpectedResult().toLowerCase().contains(criteria) || t.getDateUpdated().toLowerCase().contains(criteria) || t.getDateCreated().toLowerCase().contains(criteria)){
+                    toReturn.add(t);
+                }
+            }
+        }
+        else if(Objects.equals(selection, "name")){
+            for(TestCase t : allTests){
+                if(t.getName().toLowerCase().contains(criteria)){
+                    toReturn.add(t);
+                }
+            }
+        }
+        else if(Objects.equals(selection, "steps")){
+            for(TestCase t : allTests){
+                if(t.getSteps().toLowerCase().contains(criteria)){
+                    toReturn.add(t);
+                }
+            }
+        }
+        else if(Objects.equals(selection, "expected")){
+            for(TestCase t : allTests){
+                if(t.getExpectedResult().toLowerCase().contains(criteria)){
+                    toReturn.add(t);
+                }
+            }
+        }
+        else if(Objects.equals(selection, "create")){
+            for(TestCase t : allTests){
+                if(t.getDateCreated().toLowerCase().contains(criteria)){
+                    toReturn.add(t);
+                }
+            }
+        }
+        else if(Objects.equals(selection, "updated")){
+            for(TestCase t : allTests){
+                if(t.getDateUpdated().toLowerCase().contains(criteria)){
+                    toReturn.add(t);
+                }
+            }
+        }
+        return toReturn;
     }
 }
--- app/src/main/java/com/softwareengineering/testmanagementcapstone/database/Repository.java
+++ app/src/main/java/com/softwareengineering/testmanagementcapstone/database/Repository.java
@@ -225,4 +225,5 @@ public class Repository {
             e.printStackTrace();
         }
     }
+
 }
